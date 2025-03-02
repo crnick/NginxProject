39958b5f8a7772e3ee3ff2372a2f633714c0ed15
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-03-02 16:56:03 -0500

    added comments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
+//deliver static content to frontend
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', (req, res) => {
